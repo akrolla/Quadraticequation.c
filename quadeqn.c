609a5a1631422c2e9108c142afec0620b7663a1e
akrolla <akrolla@users.noreply.github.com>
2017-02-10 07:25:25 +0000

    Update quadeqn.c

--- quadeqn.c
+++ quadeqn.c
@@ -8,11 +8,11 @@ Quadratic Equation
 #include <stdio.h>
 #include <math.h>
 
-void solve_linear();
-void solve_quad();
-void solve_real();
+void solve_linear(int,int);
+void solve_quad(int,int,int);
+void solve_real(int,int);
 void solve_complex();
-int a; int b; int c;
+int a; int b; int c; int x;
 
 // Main program
 
@@ -20,28 +20,53 @@ int main(int argc, char*argv[])
 {
       if (argc==4)
       {
-        sscanf("%d" , a) ;
-        sscanf("%d" , b) ;
-        sscanf("%d" , c) ;
+        sscanf("%d" ,&a) ;
+        sscanf("%d" ,&b) ;
+        sscanf("%d" ,&c) ;
         
           if (a==0 && b==0)
               {
-              printf("Error. Degenerate Equation! \n");
+              printf("Error. %d and %d lead to a Degenerate Equation! \n",a ,b);
+              
+                   else
+                         {
+                        solve_linear(int a,int b,int c);
+                         }
               }
-           else
+          else
               {
-              solve_linear(a,b,c);
+              solve_quad(int a, int b, int c);
               }
-      }
-      else
-      {
-            solve_quad(a,b,c);
-      }
-            
-            
-     
-       
-}
+     }    
+}           
+                               //sub programs
+      
+      
+                              solve_real()
+                                     {
+                                    int x1; int x2; float;
+                                    
+                                    x1 = (-b + sqrt(b*b-4*a*c))/(2*a) ;
+                                    
+                                    x2 = (-b - sqrt(b*b-4*a*c))/(2*a);
+                                    
+                                           printf("%f is x1.\n",x1);
+                                           printf("%f is x2.\n ",x2);
+                                     }
+                              
+                              solve_complex()
+                                    {
+                                          int x_real; int x_imag; float ;
+                                    
+                                    x_real = -b/(2.0*a);
+                                          
+                                    x_imag = (sqrt(b*b-4*a*c))/(2*a);
+                                                
+                                                printf("%f + %fi\n", x_real , x_imag);
+                                                printf("%f - %fi\n", x_real , x_imag);
+                                    }
+                                          
+
 
 
 
